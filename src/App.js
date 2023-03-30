5dc672e78b12b43f4e25c19c1a80ed05382cb821
Tom French <tom@tomfren.ch>
2023-03-30 15:04:07 +0100

    fix: serialize acir to barretenberg circuit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { enableLogs } from '@aztec/barretenberg/log';
 import {
   setupTurboProverAndVerifier
 } from "@aztec/barretenberg";
+import { serialise_acir_to_barrtenberg_circuit } from '@noir-lang/aztec_backend';
 
 
 import './App.css';
@@ -11,12 +12,14 @@ function App() {
   enableLogs("bb:*");
   fetch(new URL('../circuit/target/circuit.json', import.meta.url)).then(r => r.json()).then(compiled_program => {
     let acir_bytes = new Uint8Array(Buffer.from(compiled_program.circuit, "hex"));
+    const serializedCircuit = serialise_acir_to_barrtenberg_circuit(acir_bytes);
+
     fetch(new URL('../circuit/target/circuit.pk', import.meta.url)).then(r => r.blob()).then(pk_blob => pk_blob.arrayBuffer()).then(pk_arraybuffer => {
       fetch(new URL('../circuit/target/circuit.vk', import.meta.url)).then(r => r.blob()).then(vk_blob => vk_blob.arrayBuffer()).then(vk_arraybuffer => {
         let pk = new Uint8Array(pk_arraybuffer);
         let vk = new Uint8Array(vk_arraybuffer);
-        console.log(acir_bytes, pk, vk)
-        setupTurboProverAndVerifier(acir_bytes, pk, vk);
+        console.log(serializedCircuit, pk, vk)
+        setupTurboProverAndVerifier(serializedCircuit, pk, vk);
       });
     })
   });
