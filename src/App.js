68390cc813844459bd57fe499f48348c4808fa6f
Koby <koby@aztecprotocol.com>
2023-03-30 18:05:36 +0200

    easier to read

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useRef, useState, useEffect } from 'react';
+
 import { enableLogs } from '@aztec/barretenberg/log';
 import {
   setupTurboProverAndVerifier
@@ -8,22 +10,39 @@ import initAztecBackend, { serialise_acir_to_barrtenberg_circuit } from '@noir-l
 import './App.css';
 
 function App() {
-  enableLogs("bb:*");
-  initAztecBackend().then(_ => {
-    fetch(new URL('../circuit/target/circuit.json', import.meta.url)).then(r => r.json()).then(compiled_program => {
-      let acir_bytes = new Uint8Array(Buffer.from(compiled_program.circuit, "hex"));
-      const serializedCircuit = serialise_acir_to_barrtenberg_circuit(acir_bytes);
 
-      fetch(new URL('../circuit/target/circuit.pk', import.meta.url)).then(r => r.blob()).then(pk_blob => pk_blob.arrayBuffer()).then(pk_arraybuffer => {
-        fetch(new URL('../circuit/target/circuit.vk', import.meta.url)).then(r => r.blob()).then(vk_blob => vk_blob.arrayBuffer()).then(vk_arraybuffer => {
-          let pk = new Uint8Array(pk_arraybuffer);
-          let vk = new Uint8Array(vk_arraybuffer);
-          console.log(serializedCircuit, pk, vk)
-          setupTurboProverAndVerifier(serializedCircuit, pk, vk);
-        });
-      })
-    });
-  });
+  const [nargoArtifacts, _setNargoArtifacts] = useState(false);
+
+  const setNargoArtifacts = (artifactsObject) => {
+    nargoArtifacts.current = artifactsObject;
+    _setNargoArtifacts(artifactsObject);
+  };
+
+  useEffect(() => {
+    const fetchArtifacts = async () => {
+      if (!nargoArtifacts) {
+        const circuitJson = await fetch(new URL('../circuit/target/circuit.json', import.meta.url)).then(r => r.json());
+
+        let acir_bytes = new Uint8Array(Buffer.from(circuitJson.circuit, "hex"));
+
+        await initAztecBackend();
+        const serializedCircuit = serialise_acir_to_barrtenberg_circuit(acir_bytes);
+
+        const pk_arrayBuffer = await fetch(new URL('../circuit/target/circuit.pk', import.meta.url)).then(r => r.blob()).then(pk_blob => pk_blob.arrayBuffer());
+        const vk_arraybuffer = await fetch(new URL('../circuit/target/circuit.vk', import.meta.url)).then(r => r.blob()).then(vk_blob => vk_blob.arrayBuffer());
+
+        let pk = new Uint8Array(pk_arrayBuffer);
+        let vk = new Uint8Array(vk_arraybuffer);
+        console.log(serializedCircuit, pk, vk)
+        setupTurboProverAndVerifier(serializedCircuit, pk, vk);
+
+      }
+    }
+
+    fetchArtifacts();
+  })
+
+  enableLogs("bb:*");
 
   return (
     <div className="App">
